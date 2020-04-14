bd33d78e9676ff35a20c53aeabec22d4b0a95583
Eric Schoenborn <cultureondemand@gmail.com>
2020-04-13 19:17:36 -0700

    removed old media files and added new files

--- myplan_content.js
+++ myplan_content.js
@@ -3,35 +3,34 @@ var slideData = [
   
  
   {
-    section: "Artist Statement",
+    section: "Kinetic Cartoon",
 //////////////////     icon: "pcy.svg",
-    header: "",
- //////////////////////////   video: "yes",
-    bkgImage: {
-      small: "eric_cade_schoenborn_project.JPG",
-      medium: "eric_cade_schoenborn_project.JPG",
-      large: "eric_cade_schoenborn_project.JPG"
-      
+header: "",
+    video: "yes",
+   bkgImage: {
+     wm: "assets/images/bkg/project.webm",
+       mp: "assets/images/bkg/project.mp4",
+      name:"1"
     },
     body: `
-<h1>My work is inspired by the backyard bonfire storytelling tradition with technology at the center of mesmerizing, immersive environments. I leverage digital tools to create analog, communal experiences with hypnotic interactivity – similar to playing with the sparks in the fire – as you are transported by a storyteller's account or musical accompaniment. My process normally begins by leaving the studio with a camera, mic and laptop and venturing into the serendipitous wilds of backroads outposts, purposefully veering from the common sense of guidebooks and mainstream destinations, like the best bonfire tales do. The media and stories I collect often manifest as browser-based storyworlds experienced across platforms from Augmented Reality projection mapping installations, VR storybooks, video edits created to inspire the adventure and wanderlust.</h1>  
+<h1>Commenting on automation and innovation culture, this kinetic sculpture interacts with a digital animation that illustrates the impact of automation on huanity ... in this case driving up unemployment.</h1>  
     `
   },
   
 
   {
-    section: "My Roots. I devour media, music, ideas",
+    section: "Online Story Offline Mechanics",
 //////////////////     icon: "pcy.svg",
     header: "",
  //////////////////////////   video: "yes",
     bkgImage: {
-      small: "mags1986.jpg",
-      medium: "mags1986.jpg",
-      large: "mags1986.jpg"
+      small: "piece.png",
+      medium: "piece.png",
+      large: "piece.png"
       
     },
     body: `
-<strong>Visual Design Inspo</strong><br />David Carson / Spike Jonze / Glen E. Friedman / Mark Gonzales / Raymond Pettibone / Andy Jenkins / Natas Kaupus   
+<strong>Laser Cut Characters and Cam System, WebGL Interactive Storyworld, Custom AR Markers, Webcam</strong><br />     
     `
   },
   
@@ -39,334 +38,20 @@ var slideData = [
   
    
   {
-    section: "More Roots. I never seperated out coding and drawing",
+    section: "Destroy Jobs with a Crank",
 //////////////////     icon: "pcy.svg",
-    header: "",
- //////////////////////////   video: "yes",
-    bkgImage: {
-      small: "loresscreen.jpg",
-      medium: "loresscreen.jpg",
-      large: "loresscreen.jpg"
+header: "",
+    video: "yes",
+   bkgImage: {
+     wm: "assets/images/bkg/piece.webm",
+       mp: "assets/images/bkg/piece.mp4",
+      name:"2"
       
     },
     body: `
 <strong>Apple Basic and early graphics programming</strong><br /> 
     `
-  },
-  
-
-
-
-  {
-    section: "My Zone. I draw and make media. Constantly. I just have to.",
-//////////////////     icon: "pcy.svg",
-    header: "",
- //////////////////////////   video: "yes",
-    bkgImage: {
-      small: "zinespreadstickers.jpg",
-      medium: "zinespreadstickers.jpg",
-      large: "zinespreadstickers.jpg"
-      
-    },
-    body: `
-<strong>Zines, drawing, flyers, sketches, making music</strong><br />This was how my friend group organized and got together daily and made work, for years. It continued to evolve with graffiti, and a whole movement of what people call makers now. Evolved as we got access to video cameras and better film cameras, instruments, computers ... money.
-    `
-  },
-  
-
- 
-   
-
-
-  {
-    section: "All mediums are evolving. I'm always learning, exploring, building style",
-//////////////////////////     icon: "pcy.svg",
-    header: "",
-    bkgImage: {
-      small: "redwoodsnight.jpg",
-      medium: "redwoodsnight.jpg",
-      large: "redwoodsnight.jpg"
-    },
-    body: `
-      `
-  },
-  
-  
-  
-
-
-  {
-    section: "Digital Tools to build Communal Analog Experiences",
-//////////////////     icon: "pcy.svg",
-    header: "",
- //////////////////////////   video: "yes",
-    bkgImage: {
-      small: "phenomenalradnessartpark.jpg",
-      medium: "phenomenalradnessartpark.jpg",
-      large: "phenomenalradnessartpark.jpg"
-      
-    },
-    body: `
- <strong>Experiential background useful. I'm always thinking about outcomes, audience and problems I'm trying to solve through my work. Normally Playful.
-
-</strong><br />After so much serious work, I want to make beautiful fun experiences. https://vimeo.com/71395335
-    `
-  },
-  
-  
-  
-
-   
-  
-  
-  
-  
-   
-  
- 
-  
-
-
-  
-  
-    {
-    section: "Collecting stories and building storytelling experiences",
-//////////////////////////     icon: "pcy.svg",
-    header: "",
-    video: "yes",
-   bkgImage: {
-     wm: "assets/images/bkg/mygoodgood.webm",
-       mp: "assets/images/bkg/mygoodgood.mp4",
-      name:"1"
-    },
-    body: `
- <strong>Adapting analog media formats and experiences.</strong><br />Interactive storyworlds like pop-up books than games bonfire storytelling,the LP consuming experience.
-`
-  },
-  
-
-
-   
-  
-  
-  
-  
-
-
-  {
-    section: "Interactive experiences across platforms",
-header: "",
-    video: "yes",
-   bkgImage: {
-     wm: "assets/images/bkg/wbwb.webm",
-       mp: "assets/images/bkg/wbwb.mp4",
-      name:"2"
-    },
-
-    body:`
- <strong>Mobile, laptop/desktop, VR, installation. Leveraging my background and training in documentation:
-  Photo, Storyfinding, Editing, Audio, Video
-</strong><br />Videography, Photography, JavaScript, WebGL, HTML5, WebRTC, Projection Mapping<br />
- `
-
-  }
-
-,
-  
-
-
-
-
-
-
-  {
-    section: "I see myself continuing to build on the browser-based storyworlds",
-header: "",
-    video: "yes",
-   bkgImage: {
-     wm: "assets/images/bkg/wb.webm",
-       mp: "assets/images/bkg/wb.mp4",
-      name:"3"
-    },
-
-    body:`
- <strong>I can run installations, VR versions, laptop/desktop, mobile, the audio can stand alone, a video edit ... what you need. </strong><br />Video, Photos, JavaScript, WebGL, WebRTC, HTML5<br />
- `
-
-  }
-
-,
-  
-
- 
- 
-  
-  
-  
-  
-  {
-    section: "I see myself taking these or cousins of these browser-based worlds into the wilds",
-//////////////////////////     icon: "pcy.svg",
-    header: "",
-    bkgImage: {
-      small: "barnface.png",
-      medium: "barnface.png",
-      large: "barnface.png"
-    },
-    body: `
-<strong>Augmented Americana is my genre, I guess.</strong><br />JavaScript, WebGL, HTML5, WebRTC, Projection Mapping<br />
-     `
-  },
-  
-
-
-  
-  
-
-
-  {
-    section: "Goal for my thesis is to zero in on a storytelling experience to test ...",
-//////////////////////////     icon: "pcy.svg",
-    header: "",
-    video: "yes",
-   bkgImage: {
-    //////////// wm: "assets/images/bkg/zubair.webm",
-       mp: "assets/images/bkg/MVI_1773.mp4",
-      name:"4"
-    },
-    body: `
-... and a clear description to describe my work. To better explore the idea of analog experiences that leverage digital tools and find a balance of story and storyform that does justice to the idea of taking this technology out of the gallery and into the community. 
-    `
-  },
-  
-
-
-  
-  
-  
-  {
-    section: "Major milestones include exploring VR, AR and paper mache animatronics more thoroughly",
-//////////////////////////     icon: "pcy.svg",
-header: "",
-    video: "yes",
-   bkgImage: {
-       mp: "assets/images/bkg/IMG_4155.mp4",
-      name:"5"
-    },
-
-    body: `
-<strong>Fun, communal, immersive.</strong><br />A sample dream is Check-E-Cheese style animatronic 3D opossums controlled by VR controller.
-     `
-  },
-  
-
-
-  /*
-  
-  {
-    section: "More milestones: I want to 3D print some robot parts to get used to this medium",
-//////////////////////////     icon: "pcy.svg",
-    header: "",
-    bkgImage: {
-      small: "animatronic.jpg",
-      medium: "animatronic.jpg",
-      large: "animatronic.jpg"
-    },
-    body: `
-<strong>It does not have to be straight animatronic off the bat, but it would be good to build something doable with Audrino, and move up to VR controller.</strong><br />First project will probably be a robot hand squeezing a stress ball. 
-     `
-  },
-  
-*/
-
-
-  {
-    section: "I'm interested in better metaphors for transmedia/interactive/multimedia/whatever teams'",
-header: "",
-    video: "yes",
-   bkgImage: {
-     wm: "assets/images/bkg/stevearmstrong.webm",
-       mp: "assets/images/bkg/stevearmstrong.mp4",
-      name:"6"
-    },
-
-    body:`
- <strong>VR, AR, Storytelling Installations, Storytelling Apps and many transmedia storyforms use the metaphor of filmmaking teams or television production to these projects due to deficiencies in code familiarity among funders and those that can best talk funder language. I would love to research the creative teams and better describe the metaphor for this work so it properly can evolve past 360 videos and disaster tourism.</strong><br />
- `
-
-  }
-
-,
-  
-
- 
-  
-  
-  {
-    section: "Through the summer and into next year I want to be fully into AR experience research",
-//////////////////////////     icon: "pcy.svg",
-    header: "",
-    bkgImage: {
-      small: "chaco21.jpg",
-      medium: "chaco21.jpg",
-      large: "chaco21.jpg"
-    },
-    body: `
-<strong>I hope to keep expanding my skills and focusing the meaning of my work.</strong><br />I want to be able to test multiple forms in sets I've built as well as natural environments and culturarly significant sites.<br />
-     `
-  }
-
-
-,
-  
-
-
-
- 
-
-
-
-  {
-    section: "I want to continue to integrate AR into live spaces and bring people together with these tools",
-header: "",
-    video: "yes",
-   bkgImage: {
-     wm: "assets/images/bkg/iiipoints2.webm",
-       mp: "assets/images/bkg/iiipoints2.mp4",
-      name:"7"
-    },
-
-    body:`
- <strong>Formalizing testing and research about how people respond to these experiences in different contexts.</strong><br />
- `
-
-  }
-
-,
-  
-
- 
-  
- 
-
-
-
-
-  
-  {
-    section: "Entering thesis year",
-//////////////////////////     icon: "pcy.svg",
-    header: "",
-    bkgImage: {
-      small: "3dglasses.jpg",
-      medium: "3dglasses.jpg",
-      large: "3dglasses.jpg"
-    },
-    body: `I intend on better explaining the purpose of my work, and focusing in on describing the work.
- 
- ` }
-
-
+  } 
 
 
 ];
